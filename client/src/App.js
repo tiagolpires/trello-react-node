075724077cf3cafa262be1fb1f00b9f201feed85
unknown <tiagolpires@hotmail.com>
2021-03-31 13:00:47 -0300

    Added Header Style

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Main from './pages/Main'
+import './style.css'
 
 function App() {
   return (
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,8 +2,11 @@ import './style.css'
 
 const index = () => {
     return (
-        <header>
-            <h1>Header</h1>
+        <header className="main-header">
+            <div className="main-header-content">
+                <h1>We focus on <br/>your Story</h1>
+                <p>Fusce laoreet arcu velit, in feugiat quam pretium sit amet. Nullam euismod augue in rhoncus blandit. Suspendisse potenti. </p>
+            </div>
         </header>
     )
 }
