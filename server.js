ad75865791be41caad30bdda4153385324283c18
unknown <tiagolpires@hotmail.com>
2021-03-31 17:19:19 -0300

    Heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const cardRoute = require('./src/routes/cardRoute')
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -12,6 +13,10 @@ app.use('/api', cardRoute)
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 app.listen(PORT, console.log(`Server running at ${PORT}`))
