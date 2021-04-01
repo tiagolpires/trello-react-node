86bfde6b16e83337467a49cc9fed73492f60740d
unknown <tiagolpires@hotmail.com>
2021-04-01 12:13:46 -0300

    Production API url

--- client/src/Api.js
+++ client/src/Api.js
@@ -1,5 +1,10 @@
+let baseUrl = 'http://localhost:3001'
+if(process.env.NODE_ENV === 'production') {
+    baseUrl = 'https://trello-react-node.herokuapp.com'
+}
+
 const createCard = async(body) => {
-    const response = await fetch("http://localhost:3001/api/card", {
+    const response = await fetch(`${baseUrl}/api/card`, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(body)
