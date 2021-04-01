68cba97e5afc3e86963e24fe2461ef24f93dea60
unknown <tiagolpires@hotmail.com>
2021-03-31 23:11:26 -0300

    Changed header text

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,8 +4,10 @@ const index = () => {
     return (
         <header className="main-header">
             <div className="main-header-content">
-                <h1>We focus on <br/>your Story</h1>
-                <p>Fusce laoreet arcu velit, in feugiat quam pretium sit amet. Nullam euismod augue in rhoncus blandit. Suspendisse potenti. </p>
+                <h1>Trello<br/>Integration</h1>
+                <p>
+                    Aplicação integrada com a REST API do Trello, feita com Node.Js no back-end e React.Js no front-end. <a target="blank" href="https://trello.com/b/5b7XlMo4/nodejs-interaction">Link para a Board no Trello.</a>
+                </p>
             </div>
         </header>
     )
